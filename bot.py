8de32b9bda42dd20d0134daafc92c1f4fcc7f1ea
The Spot <96501055+TheSpotNFT@users.noreply.github.com>
2023-03-04 10:16:18 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ async def on_ready():
 
 # listen for the contract's Minted event
 @contract.on('Minted')
-def handle_event(sender, event):
+async def handle_event(sender, event):
     # get the address of the account that minted the tokens
     minter = event['args']['minter']
 
